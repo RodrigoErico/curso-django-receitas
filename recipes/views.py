56066f8588c2e0f6943bfba173ed932dc817899e
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-06-10 09:46:16 -0300

    Removido as rotas contato e sobre.

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def contact(request):
-    return HttpResponse('Pagina Contato')
-
-
-def about(request):
-    return HttpResponse('Pagina Sobre')
